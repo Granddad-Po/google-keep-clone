be36b90b80d7ddedc9fe25f7896914f4d5f8ca77
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2022-10-16 22:49:22 +0500

    add: search input

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
-import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import {alpha, AppBar, IconButton, InputBase, Toolbar, Typography} from "@mui/material";
+import {Menu as MenuIcon, Search as SearchIcon} from '@mui/icons-material';
 import {styled} from "@mui/material/styles";
 
 
@@ -15,6 +15,47 @@ const Header = styled(AppBar)`
   height: 70px;
   box-shadow: inset 0 -1px 0 0 #dadce0;
 `;
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: '#F5F5F5',
+    marginLeft: 0,
+    height: '50px',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: '200px',
+        width: '600px',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
 
 const HeaderBar: FC<HeaderBarProps> = ({handleDrawer}) => {
     const logo = 'https://cdn-icons-png.flaticon.com/512/3320/3320975.png'
@@ -35,6 +76,15 @@ const HeaderBar: FC<HeaderBarProps> = ({handleDrawer}) => {
                     <Typography variant='h5' color='inherit' sx={{ml: '25px', fontSize: '24px'}}>
                         Notes
                     </Typography>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
                 </Toolbar>
             </Header>
     );
